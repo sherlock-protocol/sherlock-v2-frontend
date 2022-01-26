a7f4f62ffd20d68ca1d9574658a6cb8f47c87243
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-01-26 18:18:21 -0300

    add react-router dependency and routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react"
+import { Route, Routes } from "react-router-dom"
+
+import { StakingPage } from "./pages/Staking/Staking"
+import { StakingPositionsPage } from "./pages/StakingPositions/StakingPositions"
+import { ProtocolBalancePage } from "./pages/ProtocolBalance/ProtocolBalance"
+
 import styles from "./App.module.scss"
 
 function App() {
-  return <div className={styles.app}>Sherlock v2 frontend</div>
+  return (
+    <div className={styles.app}>
+      <h1>Sherlock v2</h1>
+      <Routes>
+        <Route index element={<StakingPage />} />
+        <Route path="stake" element={<StakingPage />} />
+        <Route path="positions" element={<StakingPositionsPage />} />
+        <Route path="protocol" element={<ProtocolBalancePage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
