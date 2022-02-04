845980ad86f40ad2721accab0c78c72f689c68d0
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-04 12:12:15 +0200

    feat: hook for currency amount inputs

---
+++ src/hooks/useAmountState.ts
@@ -0,0 +1,48 @@
+import { BigNumber, ethers } from "ethers"
+import React from "react"
+import { escapeRegExp } from "../utils/format"
+
+/**
+ * Regex for testing the amount against
+ */
+const amountRegex = /^\d*(?:\\[.])?\d*$/
+
+/**
+ */
+
+/**
+ * React Hook for storing a validated and sanitized
+ * currency input amount.
+ *
+ * @param decimals Number of decimals units to be used when parsing as BigNumber
+ */
+const useAmountState = (decimals: number): [string, BigNumber | null, (value: string) => void] => {
+  const [amount, setAmount] = React.useState("")
+  const [amountBN, setAmountBN] = React.useState<BigNumber | null>(null)
+
+  /**
+   * Validate and set a new amount
+   */
+  const handleSetAmount = React.useCallback(
+    (value: string) => {
+      // Replace commas with periods
+      const sanitizedValue = value.replace(/,/g, ".")
+
+      // Test the inputted amount for expected format
+      if (sanitizedValue === "" || amountRegex.test(escapeRegExp(sanitizedValue))) {
+        setAmount(sanitizedValue)
+
+        try {
+          setAmountBN(ethers.utils.parseUnits(value, decimals))
+        } catch {
+          setAmountBN(null)
+        }
+      }
+    },
+    [decimals]
+  )
+
+  return [amount, amountBN, handleSetAmount]
+}
+
+export default useAmountState
