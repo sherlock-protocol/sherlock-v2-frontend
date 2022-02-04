cd3846e1ade7a7e61e76dd0dfee5645d25af911a
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-02-04 16:22:23 -0300

    feat: claim SHER tokens from Claim page

--- src/pages/FundraisingClaim/FundraisingClaim.tsx
+++ src/pages/FundraisingClaim/FundraisingClaim.tsx
@@ -1,5 +1,5 @@
 import { BigNumber, ethers } from "ethers"
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { useAccount } from "wagmi"
 import { useSherClaimContract } from "../../hooks/useSherClaimContract"
 
@@ -18,23 +18,26 @@ export const FundraisingClaimPage = () => {
   const [claimIsActive, setClaimIsActive] = useState(false)
 
   /**
+   * Refresh claimable amount from smart contract
+   */
+  const refreshClaimableAmount = useCallback(async () => {
+    if (!accountData?.address) return
+
+    try {
+      const sherAmount = await sherClaim.getClaimableAmount(accountData?.address)
+      setClaimableAmount(sherAmount)
+    } catch (error) {
+      console.error(error)
+      setClaimableAmount(undefined)
+    }
+  }, [sherClaim, setClaimableAmount, accountData?.address])
+
+  /**
    * Fetch user's claimable amount
    */
   useEffect(() => {
-    const fetchClaimableAmount = async () => {
-      if (!accountData?.address) return
-
-      try {
-        const sherAmount = await sherClaim.getClaimableAmount(accountData?.address)
-        setClaimableAmount(sherAmount)
-      } catch (error) {
-        console.error(error)
-        setClaimableAmount(undefined)
-      }
-    }
-
-    fetchClaimableAmount()
-  }, [sherClaim, setClaimableAmount, accountData?.address])
+    refreshClaimableAmount()
+  }, [refreshClaimableAmount])
 
   /**
    * Fetch claim is active or not
@@ -53,13 +56,27 @@ export const FundraisingClaimPage = () => {
     fetchClaimIsActive()
   })
 
+  const handleClaim = useCallback(() => {
+    const claim = async () => {
+      try {
+        const txReceipt = await sherClaim.claim()
+        console.log(txReceipt)
+        await refreshClaimableAmount()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    claim()
+  }, [sherClaim, refreshClaimableAmount])
+
   const sherAmount = claimableAmount && ethers.utils.formatUnits(claimableAmount)
 
   return (
     <div>
       <h1>CLAIM</h1>
       {sherAmount && <h2>Available for claim: {sherAmount} SHER</h2>}
-      <button disabled={!claimIsActive}>
+      <button onClick={handleClaim} disabled={!claimIsActive}>
         {claimIsActive ? `Claim ${sherAmount} SHER` : "Claim is not active yet"}
       </button>
     </div>
