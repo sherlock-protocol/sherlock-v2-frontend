a1eaff3437ca9fbd7b628b43fa245abea5d66afb
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-10 00:19:48 +0200

    feat: add logotype to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ConnectButton from "../ConnectButton/ConnectButton"
 import styles from "./Header.module.scss"
+import { ReactComponent as Logotype } from "../../assets/icons/logotype.svg"
 
 /**
  * Header component including the navigation and the wallet connection.
@@ -8,7 +9,9 @@ import styles from "./Header.module.scss"
 function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.leftArea}></div>
+      <div className={styles.leftArea}>
+        <Logotype height={60} width={60} />
+      </div>
       <div className={styles.centerArea}></div>
       <div className={styles.rightArea}>
         <ConnectButton />
