dca4176a094609f4216dace8591e9695729fa831
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-17 14:42:21 +0200

    refactor: local footer shadow image

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,10 @@
 import React from "react"
-
+import { ReactComponent as FooterShadow } from "../../assets/images/footer_shadow.svg"
 import styles from "./Footer.module.scss"
 
 export const Footer: React.FC = ({ children }) => (
   <div className={styles.footer}>
-    <img
-      src="https://uploads-ssl.webflow.com/6176d669c3360a6d1836d422/61cc2b6c39c94d6772972b25_Rectangle%2089%20(1).svg"
-      loading="eager"
-      alt=""
-    />
+    <FooterShadow />
     <div className={styles.bottom}></div>
     <div className={styles.content}>{children}</div>
   </div>
