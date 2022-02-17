55886dce61ed06667be11ca5df1b1a30ca079872
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-02-17 09:28:58 -0300

    refactor: Button component to be aware of wallet connect and allowance

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,8 +2,16 @@ import React from "react"
 import cx from "classnames"
 
 import styles from "./Button.module.scss"
+import { BigNumber } from "ethers"
+import ConnectGate from "../ConnectGate/ConnectGate"
+import AllowanceGate from "../AllowanceGate/AllowanceGate"
 
 type ButtonVariant = "primary" | "secondary" | "alternate" | "cta"
+
+type Allowance = {
+  spender: string
+  amount: BigNumber
+}
 
 export type ButtonProps = {
   /**
@@ -20,12 +28,48 @@ export type ButtonProps = {
    * Button variant (@see ButtonVariant)
    */
   variant?: ButtonVariant
+
+  /**
+   * Wether the action requires to have a connected account or not.
+   * `ConnectGate` HOC will be used.
+   */
+  requiresConnected?: boolean
+
+  /**
+   * Wether the action requires certain allowance.
+   * `AllowanceGate` HOC will be used.
+   */
+  allowance?: Allowance
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick, disabled, variant = "primary" }) => {
-  return (
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  disabled,
+  variant = "primary",
+  requiresConnected = false,
+  allowance,
+}) => {
+  const ConnectGateOrEmptyFragment = requiresConnected ? ConnectGate : React.Fragment
+  const AllowanceGateOrEmptyFragment: React.FC = allowance
+    ? (children) => (
+        <AllowanceGate spender={allowance.spender} amount={allowance.amount}>
+          {children}
+        </AllowanceGate>
+      )
+    : React.Fragment
+
+  const button = (
     <button className={cx(styles.button, styles[variant])} onClick={onClick} disabled={disabled}>
       <div className={styles.content}>{children}</div>
     </button>
+  )
+
+  return disabled ? (
+    button
+  ) : (
+    <ConnectGateOrEmptyFragment>
+      <AllowanceGateOrEmptyFragment>{button}</AllowanceGateOrEmptyFragment>
+    </ConnectGateOrEmptyFragment>
   )
 }
