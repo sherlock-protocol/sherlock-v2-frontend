c3344187fe5ffbb16d02141ff95496f333f1391e
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-18 14:55:30 +0200

    fix: footer shadow positioning

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { ReactComponent as FooterShadow } from "../../assets/images/footer_shadow.svg"
+import footerShadow from "../../assets/images/footer_shadow.svg"
 import styles from "./Footer.module.scss"
 
 export const Footer: React.FC = ({ children }) => (
   <div className={styles.footer}>
-    <FooterShadow />
+    <img src={footerShadow} alt="" />
     <div className={styles.bottom}></div>
     <div className={styles.content}>{children}</div>
   </div>
