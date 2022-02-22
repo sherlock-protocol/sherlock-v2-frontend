d0c3039c973f954f99f25d69722ab1a56a641919
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-22 17:41:52 +0200

    refactor: remove test loading indicator

--- src/pages/StakingPositions/StakingPositions.tsx
+++ src/pages/StakingPositions/StakingPositions.tsx
@@ -1,16 +1,14 @@
 import React from "react"
 import ConnectGate from "../../components/ConnectGate/ConnectGate"
-import Loading from "../../components/Loading/Loading"
 import { StakingPositionsList } from "../../components/StakingPositionsList/StakingPositionsList"
 import styles from "./StakingPositions.module.scss"
 
 export const StakingPositionsPage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Loading label="Loading..." />
-      {/* <ConnectGate> */}
-      {/* <StakingPositionsList />
-      </ConnectGate> */}
+      <ConnectGate>
+        <StakingPositionsList />
+      </ConnectGate>
     </div>
   )
 }
