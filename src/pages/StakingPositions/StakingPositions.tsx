21d5237d59205e764743be9d25934f42a24f9219
Rares Stanciu <rares.stanciu@outlook.com>
2022-02-22 16:18:04 +0200

    chore: linear scan animation

--- src/pages/StakingPositions/StakingPositions.tsx
+++ src/pages/StakingPositions/StakingPositions.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import ConnectGate from "../../components/ConnectGate/ConnectGate"
+import Loading from "../../components/Loading/Loading"
 import { StakingPositionsList } from "../../components/StakingPositionsList/StakingPositionsList"
 import styles from "./StakingPositions.module.scss"
 
 export const StakingPositionsPage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <ConnectGate>
-        <StakingPositionsList />
-      </ConnectGate>
+      <Loading label="Loading..." />
+      {/* <ConnectGate> */}
+      {/* <StakingPositionsList />
+      </ConnectGate> */}
     </div>
   )
 }
