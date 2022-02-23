6e13d38785bc0e8b043dc2bd83f8754178ad2c6f
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-02-23 11:20:13 -0300

    fix: remove unused import

--- src/hooks/useSherBuyContract.ts
+++ src/hooks/useSherBuyContract.ts
@@ -1,5 +1,5 @@
 import { ethers, BigNumber } from "ethers"
-import { useCallback, useEffect, useMemo } from "react"
+import { useCallback, useMemo } from "react"
 import { useContract, useProvider, useSigner } from "wagmi"
 import SherBuyABI from "../abi/SherBuy.json"
 import { SherBuy } from "../contracts/SherBuy"
