b0d222fd178c094d52cc4b195c7899475cc1b2e5
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-03-29 11:14:49 -0300

    feat: add useTVLOverTime api hook

--- src/hooks/api/urls.ts
+++ src/hooks/api/urls.ts
@@ -1,2 +1,3 @@
 export const getFundraisePosition = (account: string) => `positions/${account}/fundraise`
 export const getStakePositions = (account: string) => `positions/${account}/staking`
+export const getTVLOverTime = () => "sherlock_stats"
---
+++ src/hooks/api/useTVLOverTime.tsx
@@ -0,0 +1,65 @@
+import { useCallback, useState } from "react"
+import { BigNumber } from "ethers"
+import axios from "axios"
+import { getTVLOverTime as getTVLOverTimeUrl } from "./urls"
+
+type TVLDataPoint = {
+  timestamp: number
+  value: BigNumber
+}
+
+type GetTVLOverTimeResponseData =
+  | {
+      ok: true
+      data: {
+        timestamp: number
+        value: string
+      }[]
+    }
+  | {
+      ok: false
+      error: string
+    }
+
+const parseResponse = (response: GetTVLOverTimeResponseData): TVLDataPoint[] | null => {
+  if (response.ok === false) return null
+
+  return response.data.map((r) => ({
+    timestamp: r.timestamp,
+    value: BigNumber.from(r.value),
+  }))
+}
+
+export const useTVLOverTime = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+  const [data, setData] = useState<TVLDataPoint[] | null>(null)
+
+  const getTVLOverTime = useCallback(async () => {
+    try {
+      setLoading(true)
+
+      const { data: responseData } = await axios.get<GetTVLOverTimeResponseData>(getTVLOverTimeUrl())
+
+      if (responseData.ok === true) {
+        setData(parseResponse(responseData))
+      } else {
+        setData(null)
+        setError(new Error(responseData.error))
+      }
+    } catch (error) {
+      console.error(error)
+      setData(null)
+      setError(error)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  return {
+    loading,
+    data,
+    error,
+    getTVLOverTime,
+  }
+}
