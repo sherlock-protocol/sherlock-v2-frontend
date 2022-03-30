f994fb8632fcc36de411fb1882c7a9c3a5c8864b
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-03-30 10:26:51 -0300

    refactor: create separate component for Chart

---
+++ src/pages/SherlockDashboard/Chart.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { AreaChart, YAxis, XAxis, Tooltip, Area } from "recharts"
+import { utils } from "ethers"
+
+import { shortenNumber } from "../../utils/units"
+
+type Props = {
+  width?: number
+  height?: number
+  data?: any[]
+}
+
+export const Chart: React.FC<Props> = ({ width, height, data }) => {
+  return (
+    <AreaChart width={width} height={height} data={data}>
+      <defs>
+        <linearGradient id="tvl" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="0%" stopColor="#8414eC" stopOpacity={0.5} />
+          <stop offset="100%" stopColor="#8414eC" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+      <YAxis
+        width={80}
+        orientation="right"
+        tick={{ fill: "white", fontSize: "12px" }}
+        tickMargin={5}
+        tickFormatter={(v) => (v > 0 ? `$ ${shortenNumber(v)}` : "")}
+      />
+      <XAxis dataKey="name" tick={{ fill: "white", fontSize: "12px" }} tickMargin={5} allowDuplicatedCategory={false} />
+      <Tooltip
+        formatter={(v: number, name: string) => [`$${utils.commify(v)}`, name.toUpperCase()]}
+        itemStyle={{ color: "#19032d" }}
+        labelStyle={{ color: "gray" }}
+      />
+      <Area type="monotone" dataKey="tvl" stroke="#8414EC" fill="url(#tvl)" fillOpacity={1} />
+    </AreaChart>
+  )
+}
--- src/pages/SherlockDashboard/SherlockDashboard.tsx
+++ src/pages/SherlockDashboard/SherlockDashboard.tsx
@@ -1,21 +1,15 @@
 import React, { useEffect } from "react"
 import { utils } from "ethers"
-import { AreaChart, Area, XAxis, YAxis, Tooltip } from "recharts"
 import { DateTime } from "luxon"
 import { Box } from "../../components/Box"
 import { Column, Row } from "../../components/Layout"
 import { Title } from "../../components/Title"
 import { Text } from "../../components/Text"
-
-import { shortenNumber } from "../../utils/units"
+import { Chart } from "./Chart"
 
 import { useTVLOverTime } from "../../hooks/api/useTVLOverTime"
 
 import styles from "./SherlockDashboardPage.module.scss"
-
-function renderCustomAxisTick() {
-  return <Text>hello</Text>
-}
 
 export const SherlockDashboardPage: React.FC = () => {
   const { getTVLOverTime, data: tvlData, loading, error } = useTVLOverTime()
@@ -41,37 +35,7 @@ export const SherlockDashboardPage: React.FC = () => {
         <Row>
           <Title>$22,056,005.00</Title>
         </Row>
-        <Row>
-          {chartData && (
-            <AreaChart width={800} height={200} data={chartData}>
-              <defs>
-                <linearGradient id="tvl" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="0%" stopColor="#8414eC" stopOpacity={0.5} />
-                  <stop offset="100%" stopColor="#8414eC" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <YAxis
-                width={80}
-                orientation="right"
-                tick={{ fill: "white", fontSize: "12px" }}
-                tickMargin={5}
-                tickFormatter={(v) => (v > 0 ? `$ ${shortenNumber(v)}` : "")}
-              />
-              <XAxis
-                dataKey="name"
-                tick={{ fill: "white", fontSize: "12px" }}
-                tickMargin={5}
-                allowDuplicatedCategory={false}
-              />
-              <Tooltip
-                formatter={(v: number, name: string) => [`$${utils.commify(v)}`, name.toUpperCase()]}
-                itemStyle={{ color: "#19032d" }}
-                labelStyle={{ color: "gray" }}
-              />
-              <Area type="monotone" dataKey="tvl" stroke="#8414EC" fill="url(#tvl)" fillOpacity={1} />
-            </AreaChart>
-          )}
-        </Row>
+        <Row>{chartData && <Chart width={800} height={200} data={chartData} />}</Row>
       </Column>
     </Box>
   )
