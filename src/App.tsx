2efec9ed920a9a1e2b8b4283588888156ef4ccf3
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-04-11 16:39:38 -0300

    feat: re-enable staking

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { FundraisingClaimPage } from "./pages/FundraisingClaim"
 import { StakingPage } from "./pages/Staking"
 import { StakingPositionsPage } from "./pages/StakingPositions"
 import { USForbiddenPage } from "./pages/USForbidden"
-import { CountdownPage } from "./pages/Countdown"
 
 import { Footer } from "./components/Footer"
 import { Header, NavigationLink } from "./components/Header"
@@ -15,18 +14,12 @@ import { routes } from "./utils/routes"
 import styles from "./App.module.scss"
 import { useFundraisePosition } from "./hooks/api/useFundraisePosition"
 import { useAccount } from "wagmi"
-import config from "./config"
-import useCountdown from "./hooks/useCountdown"
-
-const COUNTDOWN_START_TIMESTAMP = config.countdownStartTimestamp
-const COUNTDOWN_END_TIMESTAMP = config.countdownEndTimestamp
 
 function App() {
   const location = useLocation()
   const [{ data: accountData }] = useAccount()
   const { getFundraisePosition, data: fundraisePositionData } = useFundraisePosition()
   const [navigationLinks, setNavigationLinks] = useState<NavigationLink[]>([])
-  const { started, ended, secondsLeft } = useCountdown(COUNTDOWN_START_TIMESTAMP, COUNTDOWN_END_TIMESTAMP)
 
   useEffect(() => {
     if (accountData?.address) {
@@ -58,29 +51,17 @@ function App() {
     setNavigationLinks(links)
   }, [location.pathname, fundraisePositionData])
 
-  const countdownVisible = started && !ended
-
   return (
     <div className={styles.app}>
       <div className={styles.noise} />
-      <Header navigationLinks={countdownVisible ? [] : navigationLinks} />
+      <Header navigationLinks={navigationLinks} />
       <div className={styles.content}>
         <Routes>
-          {!countdownVisible ? (
-            <>
-              <Route path={routes.Stake} element={<StakingPage />} />
-              <Route path={routes.Positions} element={<StakingPositionsPage />} />
-              <Route path={routes.FundraiseClaim} element={<FundraisingClaimPage />} />
-              <Route path={routes.USForbidden} element={<USForbiddenPage />} />
-              <Route path="*" element={<Navigate replace to={routes.Stake} />} />
-            </>
-          ) : (
-            <>
-              <Route index element={<CountdownPage secondsLeft={secondsLeft} />} />
-              <Route path={routes.USForbidden} element={<USForbiddenPage />} />
-              <Route path="*" element={<Navigate replace to="/" />} />
-            </>
-          )}
+          <Route path={routes.Stake} element={<StakingPage />} />
+          <Route path={routes.Positions} element={<StakingPositionsPage />} />
+          <Route path={routes.FundraiseClaim} element={<FundraisingClaimPage />} />
+          <Route path={routes.USForbidden} element={<USForbiddenPage />} />
+          <Route path="*" element={<Navigate replace to={routes.Stake} />} />
         </Routes>
       </div>
       <Footer />
