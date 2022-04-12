8b2970de4da5d58f944bb7b1124781c56538a5c4
Evert Kors <evert@hatching.io>
2022-04-12 22:38:59 +0200

    Update csv and code generation

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,4 +1,6 @@
 import euler from "./euler"
+import squeeth_by_opyn from "./squeeth_by_opyn"
+import tempus_finance from "./tempus_finance"
 
 export type CoveredProtocolMeta = {
   id: string
@@ -15,6 +17,8 @@ type CoveredProtocolsMetas = {
 
 const coveredProtocolMetas: CoveredProtocolsMetas = {
   [euler.id]: euler,
+  [squeeth_by_opyn.id]: squeeth_by_opyn,
+  [tempus_finance.id]: tempus_finance,
 }
 
 export default coveredProtocolMetas
