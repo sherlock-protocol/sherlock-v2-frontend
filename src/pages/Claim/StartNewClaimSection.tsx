a5e022102238ef4d44bfc0efa53a7ceb24cd9a0b
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-05-25 21:14:04 +0200

    feat: disable create new claim button if account isn't protocol's agent

--- src/pages/Claim/StartNewClaimSection.tsx
+++ src/pages/Claim/StartNewClaimSection.tsx
@@ -1,18 +1,22 @@
 import React, { useCallback } from "react"
 import { ethers } from "ethers"
 import { DateTime } from "luxon"
+import { useAccount } from "wagmi"
 
 import { Box } from "../../components/Box"
-import { Button } from "../../components/Button/Button"
+import { Button } from "../../components/Button"
+import { Text } from "../../components/Text"
 import { Protocol } from "../../hooks/api/protocols"
 import { useClaimManager } from "../../hooks/useClaimManager"
 import useWaitTx from "../../hooks/useWaitTx"
+import { Column } from "../../components/Layout"
 
 type Props = {
   protocol: Protocol
 }
 
 export const StartNewClaimSection: React.FC<Props> = ({ protocol }) => {
+  const [{ data: connectedAccount }] = useAccount()
   const { waitForTx } = useWaitTx()
   const { startClaim } = useClaimManager()
 
@@ -32,9 +36,23 @@ export const StartNewClaimSection: React.FC<Props> = ({ protocol }) => {
     )
   }, [startClaim, protocol, waitForTx])
 
+  /**
+   * Only protocol's agent is allowed to start a new claim
+   */
+  const canStartNewClaim = connectedAccount?.address === protocol.agent
+
   return (
     <Box shadow={false}>
-      <Button onClick={handleStartClaimClicked}>Start new claim</Button>
+      <Column spacing="m">
+        <Text size="normal" strong>
+          {protocol.name} has no active claim.
+        </Text>
+        <Button onClick={handleStartClaimClicked} disabled={!canStartNewClaim}>
+          Start new claim
+        </Button>
+
+        {!canStartNewClaim && <Text size="small">Only the protocol's agent is allowed to start a new claim.</Text>}
+      </Column>
     </Box>
   )
 }
