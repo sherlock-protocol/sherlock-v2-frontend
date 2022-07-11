750ec8dfc5929fa0ca3160ac70f8827231cd3873
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-07-11 16:18:09 -0300

    chore: remove unused code and comments

--- src/utils/WagmiProvider.tsx
+++ src/utils/WagmiProvider.tsx
@@ -4,14 +4,8 @@ import { InjectedConnector } from "wagmi/connectors/injected"
 import { publicProvider } from "wagmi/providers/public"
 import { alchemyProvider } from "wagmi/providers/alchemy"
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc"
-
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
-import { providers } from "ethers"
 import config from "../config"
-
-// Working chain ID
-// const networkId = config.networkId
-// const localNetworkId = config.localNetworkId
 
 // API key for Alchemy project
 const alchemyApiUrl = config.alchemyApiUrl
@@ -54,32 +48,6 @@ const connectors = [
 ]
 
 const client = createClient({ provider, webSocketProvider, connectors, autoConnect: true })
-
-// Set up connectors
-// const connectors = ({ chainId }: { chainId?: number | undefined }) => {
-//   return [
-//     new InjectedConnector({ chains }),
-//     new WalletConnectConnector({
-//       chains,
-//       options: {
-//         qrcode: true,
-//         chainId: networkId,
-//         rpc: {
-//           [networkId]: alchemyApiUrlHttp,
-//         },
-//       },
-//     }),
-//   ]
-// }
-
-// const provider = ({ chainId }: { chainId?: number | undefined }) => {
-//   // Use local node if working on a development chain
-//   if (process.env.NODE_ENV === "development" && chainId && chainId === localNetworkId) {
-//     return new providers.JsonRpcProvider("http://127.0.0.1:8545")
-//   }
-
-//   return new providers.AlchemyProvider(networkId, alchemyApiKey)
-// }
 
 export const WagmiProvider: React.FC<PropsWithChildren<unknown>> = ({ children }) => (
   <WagmiConfig client={client}>{children}</WagmiConfig>
