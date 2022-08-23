6713b8b9658ba1646320e0bfa6dac416e8c96cd8
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-08-23 13:02:23 -0300

    feat: contest details page

--- src/hooks/api/contests.ts
+++ src/hooks/api/contests.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "react-query"
+import { useSignTypedData } from "wagmi"
 import { contests as contestsAPI } from "./axios"
 import { getContests as getContestsUrl } from "./urls"
 
 export type Contest = {
   id: number
   title: string
+  shortDescription: string
+  description?: string
   prizePool: number
   startDate: number // Timestamp in seconds.
   endDate: number // Timestamp in seconds.
@@ -13,6 +16,8 @@ export type Contest = {
 type GetContestsResponseData = {
   id: number
   title: string
+  short_description: string
+  description: string
   prize_pool: number
   starts_at: number
   ends_at: number
@@ -26,19 +31,38 @@ export const useContests = () =>
     return response.map((d) => ({
       id: d.id,
       title: d.title,
+      shortDescription: d.short_description,
+      description: d.description,
       prizePool: d.prize_pool,
       startDate: d.starts_at,
       endDate: d.ends_at,
     }))
   })
 
-export const contestQueryKey = (id: number) => `contest/${id}`
 export const useContest = (id: number) => {
   const { data: contests } = useContests()
+  contests?.filter((c) => c.id === id)
 
-  return useQuery<Contest | null, Error>(contestQueryKey(id), async () => {
-    contests?.filter((c) => c.id === id)
+  return { data: contests && contests.length > 0 ? contests[0] : null }
+}
 
-    return contests && contests.length > 0 ? contests[0] : null
-  })
+export const useSignContestSignupMessage = (contestId: number) => {
+  const domain = {
+    name: "Sherlock Contest",
+    version: "1",
+  }
+
+  const types = {
+    Signup: [
+      { name: "action", type: "string" },
+      { name: "contest_id", type: "uint256" },
+    ],
+  }
+
+  const value = {
+    action: "signup",
+    contest_id: contestId,
+  }
+
+  return useSignTypedData({ domain, types, value })
 }
--- src/pages/ContestDetails/ContestDetails.tsx
+++ src/pages/ContestDetails/ContestDetails.tsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useCallback } from "react"
 import { Box } from "../../components/Box"
 import { Column, Row } from "../../components/Layout"
-import { useContest } from "../../hooks/api/contests"
+import { useContest, useSignContestSignupMessage } from "../../hooks/api/contests"
 
 import styles from "./ContestDetails.module.scss"
 import aaveLogo from "../../assets/icons/aave-logo.png"
 import { Title } from "../../components/Title"
+import { Text } from "../../components/Text"
+import { commify } from "../../utils/units"
+import { DateTime } from "luxon"
+import { Button } from "../../components/Button"
+import { useSignMessage, useSignTypedData } from "wagmi"
 
 export const ContestDetails = () => {
   const { data: contest } = useContest(4)
+
+  //   const { signTypedDataAsync, ...status } = useSignContestSignupMessage(4)
+
+  //   console.log(status)
+
+  //   const sign = useCallback(async () => {
+  //     await signTypedDataAsync()
+  //   }, [signTypedDataAsync])
+
+  if (!contest) return <Text>"Loading..."</Text>
 
   return (
     <Box shadow={false} fullWidth className={styles.container}>
@@ -16,10 +31,44 @@ export const ContestDetails = () => {
         <Column>
           <img src={aaveLogo} width={100} height={100} alt="AAVE" />
         </Column>
-        <Column grow={1}>
-          <Title variant="h1">{contest?.title}</Title>
+        <Column grow={1} spacing="xl">
+          <Row>
+            <Column spacing="s">
+              <Title variant="h1">{contest.title}</Title>
+              <Text>{contest?.shortDescription}</Text>
+            </Column>
+          </Row>
+          <Row>
+            <Text variant="normal">{contest.description}</Text>
+          </Row>
         </Column>
-        <Column grow={0.2}>sidebar</Column>
+        <Column grow={0.2} spacing="xl">
+          <Row>
+            <Column>
+              <Title variant="h3">PRIZE POOL</Title>
+              <Text size="extra-large" strong>
+                {commify(contest.prizePool)} USDC
+              </Text>
+            </Column>
+          </Row>
+          <Row>
+            <Column>
+              <Title variant="h3">STARTED</Title>
+              <Text size="extra-large" strong>
+                {DateTime.fromSeconds(contest.startDate).toLocaleString(DateTime.DATE_MED)}
+              </Text>
+            </Column>
+          </Row>
+          <Row>
+            <Column>
+              <Title variant="h3">ENDS IN</Title>
+              <Text size="extra-large" strong>
+                5 days 4 hours
+              </Text>
+            </Column>
+          </Row>
+          <Row>{/* <Button onClick={sign}>SIGN UP</Button> */}</Row>
+        </Column>
       </Row>
     </Box>
   )
