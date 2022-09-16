903e423f6f1f1f1f744bfad81149d2bd8202ad14
Rares Stanciu <rcstanciu@protonmail.com>
2022-09-16 12:29:44 +0300

    refactor: replace /wait-for-block

--- src/hooks/api/useWaitForBlock.tsx
+++ src/hooks/api/useWaitForBlock.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react"
 import axios from "./axios"
-import { getWaitForBlock } from "./urls"
+import { getLastIndexedBlock } from "./urls"
 
 /**
  * Hook that exposes a method to wait for a block to be indexed.
@@ -8,13 +8,21 @@ import { getWaitForBlock } from "./urls"
 export const useWaitForBlock = () => {
   const waitForBlock = useCallback(async (blockNumber: number) => {
     try {
-      const { data: responseData } = await axios.get(getWaitForBlock(blockNumber), { timeout: 60000 })
+      let tryCount = 0
+      while (tryCount < 60) {
+        const { data: responseData } = await axios.get(getLastIndexedBlock(), { timeout: 60000 })
 
-      if (responseData.ok === true) {
-        return true
-      } else {
-        return false
+        if (responseData > blockNumber) {
+          return true
+        }
+
+        tryCount++
+
+        // Sleep for 1 second between tries
+        await new Promise((r) => setTimeout(r, 1000))
       }
+
+      return false
     } catch (error) {
       console.error(error)
       return false
