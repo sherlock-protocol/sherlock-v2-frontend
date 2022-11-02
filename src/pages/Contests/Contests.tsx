5eec2b297e8adca42c44d8c48b10fcc83e2690da
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-11-02 17:34:44 -0300

    fix: signup from contests list

--- src/pages/Contests/Contests.tsx
+++ src/pages/Contests/Contests.tsx
@@ -5,9 +5,7 @@ import { Button } from "../../components/Button"
 
 import { Column, Row } from "../../components/Layout"
 import { Title } from "../../components/Title"
-import { useSignUp } from "../../hooks/api/auditors/useSignUp"
 import { useContests } from "../../hooks/api/contests"
-import { SignUpSuccessModal } from "./SignUpSuccessModal"
 import { ActiveContests } from "./ActiveContests"
 import { AuditorSignUpModal } from "./AuditorSignUpModal"
 
@@ -16,39 +14,14 @@ import { FinishedContests } from "./FinishedContests"
 import { UpcomingContests } from "./UpcomingContests"
 import { useIsAuditor } from "../../hooks/api/auditors"
 import { useAccount } from "wagmi"
-import { ErrorModal } from "../ContestDetails/ErrorModal"
 import ConnectGate from "../../components/ConnectGate/ConnectGate"
 
 export const ContestsPage: React.FC<{}> = () => {
   const { address: connectedAddress } = useAccount()
   const { data: contests } = useContests()
-  const {
-    signUp,
-    isLoading,
-    isSuccess: signUpSuccess,
-    auditor,
-    error: signupError,
-    isError: isSignupError,
-    reset: resetSignup,
-  } = useSignUp()
   const { data: isAuditor } = useIsAuditor(connectedAddress)
   const navigate = useNavigate()
   const [signUpFormModalOpen, setSignUpFormModalOpen] = useState(false)
-  const [signUpSuccessModalOpen, setSignUpSuccessModalOpen] = useState(false)
-  const [signUpErrorModalOpen, setSignUpErrorModalOpen] = useState(false)
-
-  useEffect(() => {
-    if (signUpSuccess) {
-      setSignUpSuccessModalOpen(true)
-      setSignUpFormModalOpen(false)
-    }
-  }, [signUpSuccess])
-
-  useEffect(() => {
-    if (isSignupError && signupError) {
-      setSignUpErrorModalOpen(true)
-    }
-  }, [isSignupError, signupError, setSignUpErrorModalOpen])
 
   const handleContestClick = useCallback(
     (id: number) => {
@@ -57,10 +30,9 @@ export const ContestsPage: React.FC<{}> = () => {
     [navigate]
   )
 
-  const handleErrorModalClose = useCallback(() => {
-    setSignUpErrorModalOpen(false)
-    resetSignup()
-  }, [setSignUpErrorModalOpen, resetSignup])
+  const handleSignUpModalClose = useCallback(() => {
+    setSignUpFormModalOpen(false)
+  }, [setSignUpFormModalOpen])
 
   return (
     <Column spacing="m" className={styles.container}>
@@ -72,19 +44,13 @@ export const ContestsPage: React.FC<{}> = () => {
               <Button onClick={() => setSignUpFormModalOpen(true)}>Sign up</Button>
             </ConnectGate>
           </Row>
-          {signUpFormModalOpen && <AuditorSignUpModal closeable onClose={() => setSignUpFormModalOpen(false)} />}
         </Box>
       )}
 
       <ActiveContests contests={contests} onContestClick={handleContestClick} />
       <UpcomingContests contests={contests} onContestClick={handleContestClick} />
       <FinishedContests contests={contests} onContestClick={handleContestClick} />
-      {signUpSuccessModalOpen && auditor && (
-        <SignUpSuccessModal auditor={auditor} onClose={() => setSignUpSuccessModalOpen(false)} />
-      )}
-      {signUpErrorModalOpen && (
-        <ErrorModal reason={signupError?.fieldErrors ?? signupError?.message} onClose={handleErrorModalClose} />
-      )}
+      {signUpFormModalOpen && <AuditorSignUpModal closeable onClose={handleSignUpModalClose} />}
     </Column>
   )
 }
