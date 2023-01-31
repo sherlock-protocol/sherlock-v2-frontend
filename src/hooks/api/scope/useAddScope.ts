829cbcdf7d4592c7f3b37174ae5944c25d977035
Fran Rimoldi <francisco.rimoldi@gmail.com>
2023-01-31 08:38:37 +0100

    feat: invalidate query after adding new scope

--- src/hooks/api/scope/useAddScope.ts
+++ src/hooks/api/scope/useAddScope.ts
@@ -1,7 +1,8 @@
-import { useMutation } from "react-query"
+import { useMutation, useQueryClient } from "react-query"
 import { AxiosError } from "axios"
 import { contests as contestsAPI } from "../axios"
 import { addScope as addScopeUrl } from "../urls"
+import { scopeQueryKey } from "./useScope"
 
 type AddScopeParams = {
   protocolDashboardID: string
@@ -9,23 +10,32 @@ type AddScopeParams = {
 }
 
 export const useAddScope = () => {
+  const queryClient = useQueryClient()
+
   const {
     mutate: addScope,
     mutateAsync,
     ...mutation
-  } = useMutation<void, Error, AddScopeParams>(async (params) => {
-    try {
-      const repo_name = params.repoName.startsWith("https://github.com/")
-        ? params.repoName.replace("https://github.com/", "")
-        : params.repoName
-      await contestsAPI.post(addScopeUrl(params.protocolDashboardID), {
-        repo_name,
-      })
-    } catch (error) {
-      const axiosError = error as AxiosError
-      throw Error(axiosError.response?.data.error ?? "Something went wrong. Please, try again.")
+  } = useMutation<void, Error, AddScopeParams>(
+    async (params) => {
+      try {
+        const repo_name = params.repoName.startsWith("https://github.com/")
+          ? params.repoName.replace("https://github.com/", "")
+          : params.repoName
+        await contestsAPI.post(addScopeUrl(params.protocolDashboardID), {
+          repo_name,
+        })
+      } catch (error) {
+        const axiosError = error as AxiosError
+        throw Error(axiosError.response?.data.error ?? "Something went wrong. Please, try again.")
+      }
+    },
+    {
+      onSuccess(data, params) {
+        queryClient.invalidateQueries(scopeQueryKey(params.protocolDashboardID))
+      },
     }
-  })
+  )
 
   return {
     addScope,
