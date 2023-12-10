cb43ff30384104643b3399f89663c57c34f37d66
Fran Rimoldi <francisco.rimoldi@gmail.com>
2023-12-10 20:55:57 -0300

    fix: confirm contest timeout

--- src/hooks/api/admin/useAdminConfirmContest.ts
+++ src/hooks/api/admin/useAdminConfirmContest.ts
@@ -22,16 +22,23 @@ export const useAdminConfirmContest = () => {
   const { mutate, mutateAsync, ...mutation } = useMutation<void, Error, AdminConfirmContestParams>(
     async (params) => {
       try {
-        await contestsAPI.post(adminConfirmContestUrl(params.id), {
-          title: params.title,
-          short_description: params.shortDescription,
-          starts_at: params.startDate.toSeconds(),
-          ends_at: params.endDate.toSeconds(),
-          audit_rewards: params.auditRewards,
-          judging_prize_pool: params.judgingPrizePool,
-          lead_judge_fixed_pay: params.leadJudgeFixedPay,
-          full_payment: params.fullPayment,
-        })
+        await contestsAPI.post(
+          adminConfirmContestUrl(params.id),
+          {
+            title: params.title,
+            short_description: params.shortDescription,
+            starts_at: params.startDate.toSeconds(),
+            ends_at: params.endDate.toSeconds(),
+            audit_rewards: params.auditRewards,
+            judging_prize_pool: params.judgingPrizePool,
+            lead_judge_fixed_pay: params.leadJudgeFixedPay,
+            full_payment: params.fullPayment,
+          },
+
+          {
+            timeout: 5 * 60 * 1000,
+          }
+        )
       } catch (error) {
         const axiosError = error as AxiosError
         throw Error(axiosError.response?.data.error ?? "Something went wrong. Please, try again.")
