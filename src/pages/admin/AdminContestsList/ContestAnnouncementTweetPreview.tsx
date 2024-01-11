3532590dd0a4ef0df8463574db21f3557edef9a4
Fran Rimoldi <francisco.rimoldi@gmail.com>
2024-01-11 14:54:17 -0300

    feat: display multilpe tweets previews

--- src/pages/admin/AdminContestsList/ContestAnnouncementTweetPreview.tsx
+++ src/pages/admin/AdminContestsList/ContestAnnouncementTweetPreview.tsx
@@ -1,4 +1,5 @@
 import TweetCard from "react-tweet-card"
+import { Column } from "../../../components/Layout"
 import { useAdminContestTweetPreview } from "../../../hooks/api/admin/useAdminContestTweetPreview"
 
 type Props = {
@@ -6,17 +7,21 @@ type Props = {
 }
 
 export const ContestAnnouncementTweetPreview: React.FC<Props> = ({ contestID }) => {
-  const { data: tweet } = useAdminContestTweetPreview(contestID)
+  const { data: tweets } = useAdminContestTweetPreview(contestID)
 
   return (
-    <TweetCard
-      author={{
-        name: "Sherlock",
-        username: "sherlockdefi",
-        image: "https://pbs.twimg.com/profile_images/1436392128649646080/JbU4oAP1_400x400.jpg",
-      }}
-      tweet={tweet ?? ""}
-      time={new Date()}
-    />
+    <Column alignment="center" spacing="m">
+      {tweets?.map((t) => (
+        <TweetCard
+          author={{
+            name: "Sherlock",
+            username: "sherlockdefi",
+            image: "https://pbs.twimg.com/profile_images/1436392128649646080/JbU4oAP1_400x400.jpg",
+          }}
+          tweet={t ?? ""}
+          time={new Date()}
+        />
+      ))}
+    </Column>
   )
 }
